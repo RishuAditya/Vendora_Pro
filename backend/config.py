89b5271a71d2a898efb3971b6539b595f9dbef57
RishuAditya <therishu13@gmail.com>
2026-04-02 20:18:44 +0530

    Fixed cryptography and config import issues

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-# .env file se details load karein
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '../.env'))
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "vendora_master_secret"
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(basedir, '../frontend/static/images')
+    
+    # Cloudinary Settings
+    CLOUDINARY_CLOUD_NAME = os.environ.get("CLOUDINARY_CLOUD_NAME")
+    # API key check handling
+    CLOUDINARY_API_KEY = os.environ.get("CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET = os.environ.get("CLOUDINARY_API_SECRET")
