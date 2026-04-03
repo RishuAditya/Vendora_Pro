fc05bfb66a577199a19bb1698a25d2ec8cdc3836
RishuAditya <therishu13@gmail.com>
2026-04-03 23:39:04 +0530

    Cleaned for Koyeb deployment

--- api/index.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-path = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(path, ".."))
-
-from backend import create_app
-app = create_app()
--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from backend import create_app
 
-# Vercel ko ye variable 'app' chahiye hota hai
 app = create_app()
 
 if __name__ == "__main__":
-    # Local testing ke liye debug on rahega
-    app.run(debug=True)
+    app.run()
